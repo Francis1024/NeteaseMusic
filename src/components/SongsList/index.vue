35e4ced2159b0626948b53bc2c91f5971bd3b901
zuotaiyu <yiyboy@qq.com>
2019-12-28 18:10:31 +0800

    <feat>(<data>):播放当前音乐时按钮样式改变<add>

---
+++ src/components/SongsList/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="songs-list">
+    <ul v-if="songList.length != 0">
+      <li
+        v-for="value in songList"
+        :key="value.id"
+        @click="playMusic(value.id)"
+      >
+        <div>
+          <div class="news_txt_top">{{ value.name }}</div>
+          <div class="news_txt_bottom">
+            <div></div>
+            <div>{{ value.song.artists[0].name }} - {{ value.name }}</div>
+          </div>
+        </div>
+        <div class=".news_icon">
+          <van-icon name="play-circle-o" v-show="value.id && !value.isPlay" />
+          <van-icon name="pause-circle-o" v-show="value.id && value.isPlay" />
+        </div>
+      </li>
+    </ul>
+    <div class="tips" v-else>暂无数据</div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "SongsList",
+  props: {
+    songList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    playMusic(id) {
+      this.getMusicUrl(id).then(() => {
+        this.songList.forEach(el => {
+          if (el.id === id) {
+            el.isPlay = true;
+            return;
+          }
+          el.isPlay = false;
+        });
+        this.$forceUpdate();
+      });
+    },
+    ...mapActions(["getMusicUrl"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.songs-list {
+  ul {
+    padding-left: 6px;
+    li {
+      position: relative;
+      height: 60px;
+      border-bottom: 1px solid #ccc;
+      padding-left: 4px;
+      padding-right: 50px;
+      > div:nth-of-type(1) {
+        padding-top: 8px;
+        .news_txt_top {
+          font-size: 17px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          word-break: normal;
+          color: #000;
+          padding-bottom: 2px;
+        }
+        .news_txt_bottom {
+          display: flex;
+          align-items: center;
+          > div:nth-of-type(1) {
+            display: inline-block;
+            width: 12px;
+            height: 8px;
+            margin-right: 4px;
+            background: url("../../assets/hot_icon.png") no-repeat;
+            background-size: 166px 97px;
+          }
+          > div:nth-of-type(2) {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            word-break: normal;
+          }
+        }
+      }
+      > div:nth-of-type(2) {
+        position: absolute;
+        top: 50%;
+        right: 8px;
+        transform: translate(0, -50%);
+        > i {
+          font-size: 30px;
+          color: #ccc;
+        }
+      }
+    }
+  }
+  .tips {
+    text-align: center;
+  }
+}
+</style>
--- src/components/Tab/TabIndex.vue
+++ src/components/Tab/TabIndex.vue
@@ -28,33 +28,18 @@
           最新音乐
         </div>
       </div>
-      <div class="news_music_centent">
-        <ul>
-          <li
-            v-for="value in newSongList"
-            :key="value.id"
-            @click="getMusicUrl(value.id)"
-          >
-            <div>
-              <div class="news_txt_top">{{ value.name }}</div>
-              <div class="news_txt_bottom">
-                <div></div>
-                <div>{{ value.song.artists[0].name }} - {{ value.name }}</div>
-              </div>
-            </div>
-            <div class=".news_icon">
-              <van-icon name="play-circle-o" />
-            </div>
-          </li>
-        </ul>
-      </div>
+      <SongsList :songList="newSongList"></SongsList>
     </div>
   </div>
 </template>
 <script>
+import SongsList from "../SongsList/index";
 import { mapActions, mapGetters } from "vuex";
 import { Toast } from "vant";
 export default {
+  components: {
+    SongsList
+  },
   mounted() {
     this.getRemdSongList()
       .then(() => {
@@ -84,7 +69,7 @@ export default {
       });
   },
   methods: {
-    ...mapActions(["getRemdSongList", "getNewSongList", "getMusicUrl"])
+    ...mapActions(["getRemdSongList", "getNewSongList"])
   },
   computed: { ...mapGetters(["remdList", "newSongList"]) }
 };
@@ -148,58 +133,6 @@ export default {
       border-top: 2px solid transparent;
       padding-left: 10px;
       font-size: 16px;
-    }
-  }
-  .news_music_centent {
-    ul {
-      padding-left: 6px;
-      li {
-        position: relative;
-        height: 60px;
-        border-bottom: 1px solid #ccc;
-        padding-left: 4px;
-        padding-right: 50px;
-        > div:nth-of-type(1) {
-          padding-top: 8px;
-          .news_txt_top {
-            font-size: 17px;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            word-break: normal;
-            color: #000;
-            padding-bottom: 2px;
-          }
-          .news_txt_bottom {
-            display: flex;
-            align-items: center;
-            > div:nth-of-type(1) {
-              display: inline-block;
-              width: 12px;
-              height: 8px;
-              margin-right: 4px;
-              background: url("../../assets/hot_icon.png") no-repeat;
-              background-size: 166px 97px;
-            }
-            > div:nth-of-type(2) {
-              overflow: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-              word-break: normal;
-            }
-          }
-        }
-        > div:nth-of-type(2) {
-          position: absolute;
-          top: 50%;
-          right: 8px;
-          transform: translate(0, -50%);
-          > i {
-            font-size: 30px;
-            color: #ccc;
-          }
-        }
-      }
     }
   }
 }
