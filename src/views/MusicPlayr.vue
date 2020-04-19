f33c9f2e70a7414260a11c7acb39dbd5448b4798
Francis <yiyboy@qq.com>
2020-04-19 18:34:07 +0800

    ✅完成歌曲播放页面

--- src/views/MusicPlayr.vue
+++ src/views/MusicPlayr.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="music_playr">
-    <div ref="song-bg" class="song-bg" :style="{backgroundImage:`url(${songInfo.picUrl})`,opacity:songInfo.picUrl?'1':'0'}" />
-    <div>
-      内容部分
+    <div
+      ref="song-bg"
+      class="song-bg"
+      :style="{backgroundImage:`url(${songInfo.picUrl})`,opacity:songInfo.picUrl?'1':'0'}"
+    />
+    <div class="song-set" @click="handleSong">
+      <van-icon v-show="playBtnShow" name="play-circle-o" />
+      <div ref="setSong" class="song-set-btn">
+        <img :src="songInfo.picUrl" :alt="songInfo.name">
+      </div>
     </div>
-    <audio v-show="false" class="audio" :src="currentMusicUrl" controls autoplay>您的浏览器太老了不支持audio 标签</audio>
+    <audio
+      v-show="false"
+      ref="setAudio"
+      class="audio"
+      :src="currentMusicUrl"
+      controls
+      autoplay
+    >您的浏览器太老了不支持audio 标签</audio>
   </div>
 </template>
 <script>
@@ -15,7 +29,11 @@ export default {
   data() {
     return {
       musicId: this.$route.query.id,
-      songInfo: {}
+      playBtnShow: false,
+      songInfo: {
+        picUrl: '',
+        name: ''
+      }
     };
   },
   computed: {
@@ -30,6 +48,21 @@ export default {
       await getMusicDetail(this.musicId).then(res => {
         this.songInfo = res.songs[0].al;
       });
+    },
+    handleSong() {
+      this.$nextTick(() => {
+        if (this.$refs.setAudio.paused) {
+          this.$refs.setSong.style.animationPlayState = 'running'
+          this.playBtnShow = false
+          this.$refs.setAudio.play()
+        } else {
+          this.$refs.setSong.style.animationPlayState = 'paused'
+          this.playBtnShow = true
+          this.$refs.setAudio.pause()
+        }
+      }
+
+      );
     },
     ...mapActions(['getMusicUrl'])
   }
@@ -49,9 +82,41 @@ export default {
     background-size: auto 100%;
     transform-origin: center top;
     transform: scale(1.5);
-    transition: opacity .3s linear;
+    transition: opacity 0.3s linear;
     z-index: -1;
-    filter:blur(16px);
+    filter: blur(16px);
+  }
+  .song-set {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    i {
+      position: absolute;
+      top: 50%;
+      font-size: 1.33333rem;
+      z-index: 1;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #fff;
+    }
+    .song-set-btn {
+      animation: circle 3s infinite linear;
+      img {
+        display: block;
+        margin: 0px auto;
+        width: 200px;
+        border-radius: 50%;
+      }
+    }
+    @keyframes circle {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
   }
 }
 </style>
